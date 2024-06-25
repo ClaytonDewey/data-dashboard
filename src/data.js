b349bb86cf6d8edec692aea2327257a9a7bac5f4
ClaytonDewey <clay@dryadmedia.com>
2024-06-25 16:59:08 -0500

    Update Icons, and setup data file

---
+++ src/data.js
@@ -0,0 +1,71 @@
+import {
+  IconAnalytics,
+  IconProduct,
+  IconCustomers,
+  IconDashboard,
+  IconHelp,
+  IconInvoice,
+  IconLogout,
+  IconMessages,
+  IconPayment,
+  IconSecurity,
+  IconSettings,
+} from '../icons';
+
+export const navItems = [
+  {
+    id: 1,
+    name: 'Dashboard',
+    icon: IconDashboard,
+  },
+  {
+    id: 2,
+    name: 'Payment',
+    icon: IconPayment,
+  },
+  {
+    id: 3,
+    name: 'Customers',
+    icon: IconCustomers,
+  },
+  {
+    id: 4,
+    name: 'Messages',
+    icon: IconMessages,
+  },
+  {
+    id: 5,
+    name: 'Product',
+    icon: IconProduct,
+  },
+  {
+    id: 6,
+    name: 'Invoice',
+    icon: IconInvoice,
+  },
+  {
+    id: 7,
+    name: 'Analytics',
+    icon: IconAnalytics,
+  },
+  {
+    id: 8,
+    name: 'Settings',
+    icon: IconSettings,
+  },
+  {
+    id: 9,
+    name: 'Security',
+    icon: IconSecurity,
+  },
+  {
+    id: 10,
+    name: 'Help',
+    icon: IconHelp,
+  },
+  {
+    id: 11,
+    name: 'Logout',
+    icon: IconLogout,
+  },
+];
